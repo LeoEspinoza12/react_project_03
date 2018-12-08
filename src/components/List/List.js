7f7e4c724800f5e61ef579385a704d77761783b4
Leo espinoza <leo.spinoza@yahoo.com>
2018-12-08 14:04:19 -0600

    added the set up for component structures

---
+++ src/components/List/List.js
@@ -0,0 +1,36 @@
+
+
+import React from 'react'
+import './List.css'
+
+
+const List = () => {
+
+
+
+  return (
+    <div className='List'>
+       <ul>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+            <li>sample</li>
+          </ul>
+    </div>
+  )
+}
+
+
+export default List
---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import './Backdrop.css'
+
+
+const Backdrop = () => {
+  
+  
+
+  return (
+    <div className='Backdrop'>
+      This is the backdrop
+    </div>
+  )
+}
+
+export default Backdrop
